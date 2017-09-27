b7994f41b4499c3e81742224e3bc5c43aef817da
Alexandre Loures <alexandre.loures@ymail.com>
2017-09-27 11:07:38 -0300

    My first commit

---
+++ growth-2017/code.R
@@ -0,0 +1,25 @@
+##  ----------------------------------------  ##
+##  ordering countries in alphabetical order  ##
+##  ----------------------------------------  ##
+
+
+##  1º ARG
+##  2º BRA
+##  3º CHN
+##  4º COL
+##  5º IND
+##  6º IDN
+##  7º MYS
+##  8º MEX
+##  9º PER
+## 10º PHL
+## 11º ZAF
+## 12º THA
+## 13º TUR
+
+
+sample (1:13, 1, replace = FALSE)
+
+
+## the number reported will be the countries eliminated
+## in the present study the routine reported 12. Therefore, THA is eliminated 
